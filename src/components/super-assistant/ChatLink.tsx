85deccfba99a6894f31564c9af44f9308d081c1c
Julie <julie.terrassier@epfl.ch>
2024-07-29 18:31:34 +0200

    Grey chat bubbles

--- src/components/super-assistant/ChatLink.tsx
+++ src/components/super-assistant/ChatLink.tsx
@@ -14,7 +14,7 @@ export function ChatLink({
   return (
     <div className="block shadow-lg transition hover:scale-105 hover:shadow-2xl group rounded-3xl">
       <div className="relative pb-[57.14%]">
-        <div className="rounded-3xl overflow-hidden absolute inset-0 focus-within:ring-8 bg-sky-400 hover:bg-sky-700">
+        <div className="rounded-3xl overflow-hidden absolute inset-0 focus-within:ring-8 bg-slate-500 hover:bg-slate-700">
           <Link
             href={href}
             className="absolute inset-0 flex p-4 text-white items-end focus:outline-none"
