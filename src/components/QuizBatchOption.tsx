cd7ec07dd6948f0a513dc83f6d995a5f21f1c907
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-09-08 13:12:16 +0200

    Create QuizBatchOption

---
+++ src/components/QuizBatchOption.tsx
@@ -0,0 +1,31 @@
+import { Switch } from "@headlessui/react";
+import clsx from "clsx";
+import Tooltip from "./Tooltip";
+
+export function QuizBatchOption({
+  checked,
+  onChange,
+  tooltip,
+  children,
+}: React.PropsWithChildren<{
+  checked: boolean;
+  onChange: (newValue: boolean) => void;
+  tooltip: React.ReactNode;
+}>) {
+  return (
+    <Tooltip tip={tooltip} side="bottom" sideOffset={4} asChild>
+      <Switch
+        className={clsx(
+          "p-1.5 rounded mr-1 transition-colors",
+          checked
+            ? "bg-slate-300 text-slate-700"
+            : "hover:bg-slate-200 hover:text-slate-600 text-slate-500",
+        )}
+        checked={checked}
+        onChange={onChange}
+      >
+        {children}
+      </Switch>
+    </Tooltip>
+  );
+}
