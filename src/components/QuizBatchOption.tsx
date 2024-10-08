2e9b7e940884d62d437e4c2842ca2ea95705054e
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-10-08 13:55:30 +0200

    Allow QuizBatchOption to be disabled

--- src/components/QuizBatchOption.tsx
+++ src/components/QuizBatchOption.tsx
@@ -7,22 +7,27 @@ export function QuizBatchOption({
   onChange,
   tooltip,
   children,
+  disabled = false,
 }: React.PropsWithChildren<{
   checked: boolean;
   onChange: (newValue: boolean) => void;
   tooltip: React.ReactNode;
+  disabled?: boolean;
 }>) {
   return (
     <Tooltip tip={tooltip} side="bottom" sideOffset={4} asChild>
       <Switch
         className={clsx(
           "p-1.5 rounded mr-1 transition-colors focus:outline-none focus-visible:ring",
+          "disabled:opacity-50 disabled:cursor-not-allowed",
           checked
             ? "bg-slate-300 text-slate-700"
-            : "hover:bg-slate-200 hover:text-slate-600 text-slate-500",
+            : !disabled &&
+                "hover:bg-slate-200 hover:text-slate-600 text-slate-500",
         )}
         checked={checked}
         onChange={onChange}
+        disabled={disabled}
       >
         {children}
       </Switch>
