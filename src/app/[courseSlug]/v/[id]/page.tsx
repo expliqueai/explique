a8f6b352eba821d0a54aae0b70fc475acf007a04
Mw3y <maxence1604.minecraft@gmail.com>
2025-02-19 21:28:10 +0100

    Add chat dummy to video page

--- src/app/[courseSlug]/v/[id]/page.tsx
+++ src/app/[courseSlug]/v/[id]/page.tsx
@@ -7,19 +7,25 @@ export default function VideoPage() {
   const playerRef = useRef<ReactPlayer>(null);
 
   return (
-    <div className="flex justify-center mt-8">
-      <div className="overflow-hidden rounded-xl shadow-lg">
-        <ReactPlayer
-          ref={playerRef}
-          url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
-          config={{
-            playerVars: {
-              color: "white",
-            },
-          }}
-          controls
-        />
+    <div className="flex flex-col xl:flex-row h-screen p-4 xl:p-8 space-y-4 xl:space-y-0 xl:space-x-8">
+      <div className="xl:flex-1">
+        <div className="overflow-hidden rounded-xl shadow-lg relative">
+          <ReactPlayer
+            ref={playerRef}
+            url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+            style={{ aspectRatio: "16 / 9" }}
+            config={{
+              playerVars: {
+                color: "white",
+              },
+            }}
+            controls
+            width="100%"
+            height="100%"
+          />
+        </div>
       </div>
+      <div className="xl:min-w-[65ch] rounded-xl bg-blue-100 p-4 overflow-y-auto h-full"></div>
     </div>
   );
 }
