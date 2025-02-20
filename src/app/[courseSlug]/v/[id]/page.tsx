4318d9fd77330946b8bf082f30a033f691f90de4
Max <maxence1604.minecraft@gmail.com>
2025-02-20 16:46:37 +0100

    Use m3u8 file to directly stream the video from mediaspace to the player

--- src/app/[courseSlug]/v/[id]/page.tsx
+++ src/app/[courseSlug]/v/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useRef } from "react";
-import ReactPlayer from "react-player/youtube";
+import ReactPlayer from "react-player/file";
 
 export default function VideoPage() {
   const playerRef = useRef<ReactPlayer>(null);
@@ -12,13 +12,8 @@ export default function VideoPage() {
         <div className="overflow-hidden rounded-xl shadow-lg relative">
           <ReactPlayer
             ref={playerRef}
-            url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+            url="https://streaming.cast.switch.ch/hls/p/113/sp/11300/serveFlavor/entryId/0_tfx5i6lk/v/2/ev/3/flavorId/0_mb7mr3wa/name/a.mp4/index.m3u8"
             style={{ aspectRatio: "16 / 9" }}
-            config={{
-              playerVars: {
-                color: "white",
-              },
-            }}
             controls
             width="100%"
             height="100%"
