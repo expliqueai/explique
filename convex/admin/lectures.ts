638a82ebbc9ded62bf4df8b35228ae7d4e25f9d7
Mw3y <maxence1604.minecraft@gmail.com>
2025-03-16 20:11:42 +0100

    Modify lectures for new processing pipeline

--- convex/admin/lectures.ts
+++ convex/admin/lectures.ts
@@ -6,7 +6,7 @@ import {
   internalQuery,
 } from "../_generated/server";
 import { internal } from "../_generated/api";
-import { lectureSchema } from "../schema";
+import { LECTURE_STATUS, lectureSchema } from "../schema";
 import {
   actionWithAuth,
   mutationWithAuth,
@@ -15,6 +15,33 @@ import {
 import { getCourseRegistration } from "../courses";
 import { Id } from "../_generated/dataModel";
 import { getImageForLecture } from "../lectures";
+import OpenAI from "openai";
+
+const SYSTEM_PROMPT = `
+You are an AI language model designed to assist users in navigating and understanding the content of a video. Your capabilities include answering questions about specific moments in the video using the preprocessed data provided. You can also suggest insightful questions to help users explore the video content more deeply.
+
+Instructions for User Interaction:
+- Allow the user to ask questions related to specific timestamps in the video.
+- Utilize the processed video and audio data efficiently to answer inquiries.
+- Provide clear, concise, and informative answers based on the content at the specified timestamps.
+- Offer to guide the user to related or relevant segments of the video if needed.
+- Suggest potential questions the user may find interesting on the topics of the video.
+- Do not describe what's happening on the video segment except if it is useful for your answer. The user is watching the video, so he already knows.
+- Do not answer a question that is not related to the video or to the subject of the video. (e.g.: Make a react component...)
+- Make your answers as concise as possible.
+- Before answering, check if the segment contains an error. If so, ONLY provide the next timestamp where you can answer and say that you cannot safely provide an answer because there was an error in the video processing.
+
+Important: You MUST put timestamps inside <timestamp> and </timestamp> tags. NEVER mention anything about the preprocessed video segments, events and slides. These are keywords used internally by you to differenciate data.
+
+Ensure all interactions are concise and accurate, based on the comprehensive preprocessed data, while maintaining a friendly and helpful tone to assist the user in understanding the video's educational content. Strive for brevity while ensuring all information is rooted in the data above, fostering a deeper understanding of the video’s educational content through thoughtful dialog. Use LaTeX for any necessary mathematical expressions to maintain clarity and precision.
+
+---
+
+Video data:
+
+Important: The timestamp of the beginning of a segment is inclusive but the timestamp of the end of a segment is exclusive.
+
+`;
 
 export const get = queryWithAuth({
   args: {
@@ -168,12 +195,80 @@ export const create = actionWithAuth({
       lecture,
     );
 
-    await ctx.scheduler.runAfter(0, internal.admin.videoProcessing.default, {
+    await ctx.scheduler.runAfter(0, internal.admin.lectures.processVideo, {
       lectureId: id,
       ...lecture,
     });
 
     return id;
+  },
+});
+
+export const processVideo = internalAction({
+  args: {
+    lectureId: v.id("lectures"),
+    url: v.string(),
+  },
+  handler: async (ctx, { lectureId, url }) => {
+    try {
+      // Check for the processing URL environment variable
+      const processingUrl = process.env.LECTURES_PROCESSING_URL;
+      if (!processingUrl) {
+        throw new Error(
+          "LECTURES_PROCESSING_URL environment variable is not configured",
+        );
+      }
+
+      const response = await fetch(processingUrl, {
+        method: "POST",
+        body: JSON.stringify({
+          url,
+          convexUrl: process.env.CONVEX_CLOUD_URL,
+          lectureId,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok || !response.body) {
+        throw new Error(
+          `Failed to start video processing: ${response.statusText}`,
+        );
+      }
+
+      await ctx.runMutation(internal.admin.lectures.setStatus, {
+        lectureId,
+        status: "PROCESSING",
+      });
+    } catch (error) {
+      await ctx.runMutation(internal.admin.lectures.setStatus, {
+        lectureId,
+        status: "FAILED",
+      });
+
+      throw error;
+    }
+  },
+});
+
+export const createAssistant = internalMutation({
+  args: {
+    lectureId: v.id("lectures"),
+  },
+  handler: async (ctx, { lectureId }) => {
+    const lecture = await ctx.db.get(lectureId);
+    if (!lecture) {
+      throw new ConvexError("Lecture not found");
+    }
+
+    const openai = new OpenAI();
+    const assistant = await openai.beta.assistants.create({
+      instructions: SYSTEM_PROMPT + lecture.chunks.join("\n"),
+      model: "gpt-4o",
+    });
+
+    return await ctx.db.patch(lectureId, { assistantId: assistant.id });
   },
 });
 
@@ -187,7 +282,17 @@ export const setAssistantId = internalMutation({
   },
 });
 
-export const addChunkToVideo = internalMutation({
+export const setStatus = internalMutation({
+  args: {
+    lectureId: v.id("lectures"),
+    status: LECTURE_STATUS,
+  },
+  handler: async (ctx, { lectureId, status }) => {
+    return await ctx.db.patch(lectureId, { status: status });
+  },
+});
+
+export const addChunk = internalMutation({
   args: {
     lectureId: v.id("lectures"),
     chunk: v.string(),
