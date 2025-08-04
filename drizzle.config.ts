82ac7e8822139ea2e7ea53b159efad3c753b982b
Julie <julie.terrassier@epfl.ch>
2025-08-04 16:10:21 +0200

    fix: went back to an older version of drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,9 @@
-// @ts-nocheck
-
 import type { Config } from "drizzle-kit";
 export default {
   schema: "./drizzle/schema.ts",
-  out:    "./drizzle/migrations/",
-  driver: "better-sqlite", 
+  out: "./drizzle/migrations/",
+  driver: "better-sqlite",
   dbCredentials: {
-    file: "./drizzle/storage/sqlite.db",
+    url: "./drizzle/storage/sqlite.db",
   },
 } satisfies Config;
