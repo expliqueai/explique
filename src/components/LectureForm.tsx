3114296dc4086d8b760331267a1cda4ec60e7033
Max <maxence1604.minecraft@gmail.com>
2025-09-11 10:21:37 +0200

    fix(ui): allow lecture name to be edited

--- src/components/LectureForm.tsx
+++ src/components/LectureForm.tsx
@@ -124,7 +124,6 @@ export default function LectureForm({
         onChange={setName}
         placeholder="Bogo Sort"
         required
-        disabled={type === "update"}
       />
 
       {weeks && (
