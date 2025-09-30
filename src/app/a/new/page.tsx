c1fbcf99604660c592b724752d9631a7bb56631f
Max <maxence1604.minecraft@gmail.com>
2025-09-30 15:31:27 +0200

    fix: do not redirect user to home page when creating a new attempt

--- src/app/a/new/page.tsx
+++ src/app/a/new/page.tsx
@@ -1,32 +1,30 @@
-"use client";
+"use client"
 
-import { useEffect, useRef } from "react";
-import { api } from "../../../../convex/_generated/api";
-import { Id } from "../../../../convex/_generated/dataModel";
-import { useRouter } from "next/navigation";
-import { useAction } from "@/usingSession";
+import { useAction } from "@/usingSession"
+import { useRouter } from "next/navigation"
+import { useEffect, useRef } from "react"
+import { api } from "../../../../convex/_generated/api"
+import { Id } from "../../../../convex/_generated/dataModel"
 
 export default function Page() {
-  const router = useRouter();
-  const startAttempt = useAction(api.attempts.start);
+  const router = useRouter()
+  const startAttempt = useAction(api.attempts.start)
 
-  const executed = useRef(false);
+  const executed = useRef(false)
   useEffect(() => {
     const exerciseIdStr =
-      new URLSearchParams(window.location.search).get("exerciseId") ?? null;
+      new URLSearchParams(window.location.search).get("exerciseId") ?? null
     if (executed.current || !exerciseIdStr) {
-      return;
+      return
     }
-    executed.current = true;
-
-    (async () => {
+    executed.current = true
+    ;(async () => {
       const newAttemptId = await startAttempt({
         exerciseId: exerciseIdStr as Id<"exercises">,
-      });
-      router.replace(`/a/${newAttemptId}`);
-      window.location.href = "/";
-    })();
-  });
+      })
+      router.replace(`/a/${newAttemptId}`)
+    })()
+  })
 
-  return null;
+  return null
 }
